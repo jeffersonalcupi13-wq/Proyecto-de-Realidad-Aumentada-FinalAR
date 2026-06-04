0d454de52eed27de8c1d9e3ce1c1192e9930b7aa
Andres Dominguez <jeffersonalcupi13@gmail.com>
2026-06-04 12:45:30 -0500

    1

---
+++ My project/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (cam == null) return;
+
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward,
+                         cam.transform.rotation * Vector3.up);
+    }
+}
